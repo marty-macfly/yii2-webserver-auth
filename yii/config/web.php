3eb50f4e195a2fab6864f1bc968bb07338346b9e
Charles DELFLY <c.delfly@linkbynet.com>
2017-10-23 14:49:04 +0700

    Move controller in src

--- yii/config/web.php
+++ yii/config/web.php
@@ -48,7 +48,7 @@ $config = [
     ],
     'modules' => [
         'nginx'  => [
-            'class'       => 'macfly\nginxauth\Module',
+            'class' => 'macfly\nginxauth\Module',
         ],
     ],
     'params' => $params,
@@ -59,8 +59,7 @@ if (YII_ENV_DEV) {
     $config['bootstrap'][] = 'debug';
     $config['modules']['debug'] = [
         'class' => 'yii\debug\Module',
-        // uncomment the following to add your IP if you are not connecting from localhost.
-        //'allowedIPs' => ['127.0.0.1', '::1'],
+        'allowedIPs' => ['*'],
     ];
 
     $config['bootstrap'][] = 'gii';
