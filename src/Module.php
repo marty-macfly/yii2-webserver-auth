0b83b06cf7c176830caaf1309e83a2338d3c907b
Charles DELFLY <c.delfly@linkbynet.com>
2017-10-23 15:27:31 +0700

    Disable session and use json

--- src/Module.php
+++ src/Module.php
@@ -6,4 +6,15 @@ use Yii;
 
 class Module extends \yii\base\Module
 {
+    public function init()
+    {
+        parent::init();
+
+        if (Yii::$app->has('user')) {
+            Yii::$app->user->enableSession = false;
+        }
+
+        // Default reply format is json
+        Yii::$app->response->format = Response::FORMAT_JSON;
+    }
 }
