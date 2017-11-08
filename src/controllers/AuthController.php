c4faa58b5e78b31f3333310807f17aac783933bd
Charles DELFLY <c.delfly@linkbynet.com>
2017-11-08 16:28:44 +0700

    Make it compatible yii 2.1 and 2.0

--- src/controllers/AuthController.php
+++ src/controllers/AuthController.php
@@ -3,17 +3,19 @@
 namespace macfly\nginxauth\controllers;
 
 use Yii;
+use yii\helpers\ArrayHelper;
 
 class AuthController extends \yii\rest\Controller
 {
     public function actionIndex()
     {
-        if (Yii::$app->user->isGuest) { // User has not been authenticated yet
+        $user = Yii::$app->user;
+        if ($user->isGuest) { // User has not been authenticated yet
             $token = null;
 
-            if (Yii::$app->user->enableAutoLogin && ($token = Yii::$app->request->cookies->getValue(Yii::$app->user->identityCookie)) !== null) {
+            if ($user->enableAutoLogin && ($token = Yii::$app->request->cookies->getValue(ArrayHelper::getValue($user->identityCookie, 'name', '_identity'))) !== null) {
                 // Check if cookie-based login is use and identityCookie exist
-                Yii::info(sprintf("Cookie name '%s' found", Yii::$app->user->identityCookie));
+                Yii::info(sprintf("Cookie name '%s' found", $user->identityCookie));
             } elseif (($token = Yii::$app->request->cookies->getValue($this->module->cookie_token_name)) !== null) {
                 // Check if our own cookie exist
                 Yii::info(sprintf("Cookie name '%s' found", $this->module->cookie_token_name));
@@ -23,16 +25,19 @@ class AuthController extends \yii\rest\Controller
             }
 
             if ($token !== null) {
-                Yii::$app->user->loginByAccessToken($token);
+                $user->loginByAccessToken($token);
             }
         }
 
-        if (Yii::$app->user->isGuest == false) {
+        if ($user->isGuest == false) {
             // Check if the user has one of the permissions provided
-            if (Yii::$app->user->getAccessChecker() !== null && ($permissions = Yii::$app->request->get('permission')) !== null) {
+            if (($permissions = Yii::$app->request->get('permission')) !== null
+                && (($user->hasProperty('authManager') && $user->authManager !== null)
+                    || ($user->hasProperty('accessChecker') && $user->accessChecker !== null))
+                ) {
                 $permissions = is_array($permissions) ? $permissions : [$permissions];
                 foreach ($permissions as $permission) {
-                    if (Yii::$app->user->can($permission)) {
+                    if ($user->can($permission)) {
                         return Yii::$app->response->statusCode = 200;
                     }
                 }
