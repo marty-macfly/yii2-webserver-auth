2e7d7d02967ca0f8e3d47b1d5684ba4cf63307c1
Charles DELFLY <c.delfly@linkbynet.com>
2017-11-08 14:56:44 +0700

    If user is already logged in do not try to authenticate again (to allow controller access to be override).

--- src/controllers/AuthController.php
+++ src/controllers/AuthController.php
@@ -8,33 +8,37 @@ class AuthController extends \yii\rest\Controller
 {
     public function actionIndex()
     {
-        $token = null;
+        if (Yii::$app->user->isGuest) { // User has not been authenticated yet
+            $token = null;
 
-        if (Yii::$app->user->enableAutoLogin && ($token = Yii::$app->request->cookies->getValue(Yii::$app->user->identityCookie)) !== null) {
-            // Check if cookie-based login is use and identityCookie exist
-            Yii::info(sprintf("Cookie name '%s' found", Yii::$app->user->identityCookie));
-        } elseif (($token = Yii::$app->request->cookies->getValue($this->module->cookie_token_name)) !== null) {
-            // Check if our own cookie exist
-            Yii::info(sprintf("Cookie name '%s' found", $this->module->cookie_token_name));
-        } elseif (($token = Yii::$app->request->headers->get('x-sso-token')) !== null) {
-            // Check if the header exist
-            Yii::info(sprintf("Header name '%s' found", $this->module->cookie_token_name));
+            if (Yii::$app->user->enableAutoLogin && ($token = Yii::$app->request->cookies->getValue(Yii::$app->user->identityCookie)) !== null) {
+                // Check if cookie-based login is use and identityCookie exist
+                Yii::info(sprintf("Cookie name '%s' found", Yii::$app->user->identityCookie));
+            } elseif (($token = Yii::$app->request->cookies->getValue($this->module->cookie_token_name)) !== null) {
+                // Check if our own cookie exist
+                Yii::info(sprintf("Cookie name '%s' found", $this->module->cookie_token_name));
+            } elseif (($token = Yii::$app->request->headers->get('x-sso-token')) !== null) {
+                // Check if the header exist
+                Yii::info(sprintf("Header name '%s' found", $this->module->cookie_token_name));
+            }
+
+            if ($token !== null) {
+                Yii::$app->user->loginByAccessToken($token);
+            }
         }
 
-        if ($token !== null) {
-            if (Yii::$app->user->loginByAccessToken($token) !== null) { // Check if user is valid
-                // Check if the user has one of the permissions provided
-                if (Yii::$app->user->getAccessChecker() !== null && ($permissions = Yii::$app->request->get('permission')) !== null) {
-                    $permissions = is_array($permissions) ? $permissions : [$permissions];
-                    foreach ($permissions as $permission) {
-                        if (Yii::$app->user->can($permission)) {
-                            return Yii::$app->response->statusCode = 200;
-                        }
+        if (Yii::$app->user->isGuest == false) {
+            // Check if the user has one of the permissions provided
+            if (Yii::$app->user->getAccessChecker() !== null && ($permissions = Yii::$app->request->get('permission')) !== null) {
+                $permissions = is_array($permissions) ? $permissions : [$permissions];
+                foreach ($permissions as $permission) {
+                    if (Yii::$app->user->can($permission)) {
+                        return Yii::$app->response->statusCode = 200;
                     }
-                    throw new \yii\web\ForbiddenHttpException;
-                } else {
-                    return Yii::$app->response->statusCode = 200;
                 }
+                throw new \yii\web\ForbiddenHttpException;
+            } else {
+                return Yii::$app->response->statusCode = 200;
             }
         }
 
