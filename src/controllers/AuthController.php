565affc64956245b50d81c16996b07de41beabb1
Charles DELFLY <c.delfly@linkbynet.com>
2017-11-27 14:43:40 +0700

    test if permission is empty

--- src/controllers/AuthController.php
+++ src/controllers/AuthController.php
@@ -29,6 +29,7 @@ class AuthController extends \yii\rest\Controller
         if ($user->isGuest === false) {
             // Check if the user has one of the permissions provided
             if (($permissions = Yii::$app->request->get('permission')) !== null
+                && !empty($permissions)
                 && (($user->hasProperty('authManager') && $user->authManager !== null)
                     || ($user->hasProperty('accessChecker') && $user->accessChecker !== null))
                 ) {
